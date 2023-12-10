6f12442c60b598a51991a46c0a04b5794f57237d
Fernando <ferd.fernando@ciencias.unam.mx>
2023-12-09 21:33:45 -0600

    Calificación práctica03 equipo08

--- Practica03/Ejercicio2.sql
+++ Practica03/Ejercicio2.sql
@@ -1,52 +1,63 @@
 -- EJERCICIO 02
 
 -- (a) Detalles de productos (Nombre, Precio, Color y Tamaño)
+-- Bien
 SELECT sNombreProducto, nPrecioProducto, sColorProducto, sTamanoProducto
 FROM Productos;
 
 -- (b) Detalles de productos (Nombre, Precio, Color y Detalles) con tamaño Mediano
+-- hay que tener cuidado, en este caso se llama la columna sdescripcionproducto
 SELECT sNombreProducto, nPrecioProducto, sColorProducto, sDetallesProducto
 FROM Productos
 WHERE sTamanoProducto = 'Mediano';
 
 -- (c) Información de Clientes (Nombre, Género, Edad, Usuario y Contraseña)
+-- Bien
 SELECT sNombre, sGenero, nEdad, sUsuario, sContrasena 
 FROM clientes;
 
 -- (d) Atributos de Clientes con edad diferente a 24 años
+-- Bien
 SELECT * 
 FROM clientes
 WHERE nEdad <> 24;
 
 -- (e) Descripción de códigos de productos
+-- Bien pero se debe especificar a qué tabla de hace referencia en el ON 
 SELECT nIdProducto, sDescripcion
 FROM productos
 JOIN CCodigosProducto ON nIdProducto = nIdProducto;
 
 -- (f) Promedio de precios de productos Amarillos
+-- Bien
 SELECT AVG(productos.nPrecioProducto) AS PromedioPrecio
 FROM productos
 WHERE scolorproducto = 'Amarillo';
 
 -- (j) Contar personas sin 20, 25 ni 40 años
+-- Bien
 SELECT COUNT(*) AS CantidadPersonas
 FROM clientes
 WHERE nedad NOT IN (20, 25, 40);
 
 -- (k) Datos de Clientes cuyo apellido paterno o materno es Pérez
+-- Bien
 SELECT sNombre, sApellidoPaterno, sApellidoMaterno, sUsuario
 FROM Clientes
 WHERE sApellidoPaterno = 'Pérez' OR sApellidoMaterno = 'Pérez';
 
 -- (l) Información de órdenes (Identificador, Fecha y Notas)
+-- Bien
 SELECT nIdOrden, dFecha, sNotas
 FROM Ordenes;
 
 -- Consulta (g): Contar la cantidad total de órdenes
+-- Bien
 SELECT COUNT(*) AS CantidadOrdenes
 FROM ordenes;
 
 -- Consulta (h): Obtener nombre y edad de clientes entre 20 y 40 años
+-- Bien
 SELECT sNombre, nEdad
 FROM clientes
 WHERE nEdad BETWEEN 20 AND 40;
--- README.md
+++ README.md
@@ -6,3 +6,20 @@
 + Salmorán Acuña Jonathan
 
 Fue una práctica bastante dificl ya que desde la instalacion tuvimos problemas con la compatibilidad en nuestros dispositivos, además de que habían muchos errores con la base de datos, probablemente tengamos alguna equivocación pero eran a causa de problemas son nuestro software que por no entender bien que hacer, espeor lo tomen a consideración
+
+
+## Observaciones:
+
+### Ejercicio01
+Bien
+
+### Ejercicio02
+Bien
+
+## Calificacion:
+
+- Ejercicio01 (2 pts): 2
+
+- Ejercicio02 (8 pts): 8
+
+### Total: 10
